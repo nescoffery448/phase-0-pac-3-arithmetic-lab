41b4b137fe8c5579864f905670ab471543305160
Nyanibah Escoffery <naomi@diamondrecovery.net>
2023-09-20 06:35:27 -0500

    done

---
+++ index.js
@@ -0,0 +1,63 @@
+1 + 80; //=> 81
+60 - 40; //=> 20
+2 * 3.4; //=> 6.8
+5.0 / 2.5; //=> 2
+function add() {
+    // we'll fill this in shortly
+  }
+  function subtract() {
+    // we'll fill this in shortly
+  }
+  function multiply() {
+    // we'll fill this in shortly
+  }function divide() {
+    // we'll fill this in shortly
+  }
+  function add() {
+    return "something";
+  }
+  function add(a, b) {
+    return "something";
+  }
+  function add(a, b) {
+    return a + b;
+  }
+  function subtract() {
+    return "something";
+  }
+  function subtract(a, b) {
+    return "something";
+  }
+  function subtract(a, b) {
+    return a - b;
+  }
+  function multiply() {
+    return "something";
+  }
+  function multiply(a, b) {
+    return "something";
+  }
+  function multiply(a, b) {
+    return a * b;
+  }
+  function divide() {
+    return "something";
+  }
+  function divide(a, b) {
+    return "something";
+  }
+  function divide(a, b) {
+    return a / b;
+  }
+  function increment(n) {
+    return ++n
+  }
+  function decrement(n) {
+    return --n
+  }
+  function makeInt(n) {
+    return parseInt(n,10)
+  }
+  function preserveDecimal(n) {
+    return parseFloat(n)
+  }
